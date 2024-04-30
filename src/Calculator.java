532fa48890c53a650de93c339e39f30f0089f7d8
2minus <blecker0610@gmail.com>
2024-04-30 22:19:19 +0900

    일단 정상 작동 ) Lv2.9 : 클래스 4가지 분기 + 주석 추가

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,21 +2,21 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Calculator {
-
+    // 콜렉션 선언
     private Queue<Double> numQ = new LinkedList<Double>();
     private Queue<Double> circleQ = new LinkedList<Double>();
-
+    // 클래스 가져오기
     private final ArithmeticCalculator arithCalc = new ArithmeticCalculator();
     private final CircleCalculator circleCalc = new CircleCalculator();
-
+    // 연산 메소드
     public double arithC (double a, double b, char c) throws Exception {
         return this.arithCalc.operate(a, b, c);
     }
-
     public double circleC (double a) {
         return this.circleCalc.operate(a);
     }
-
+    // 각 콜렉션에 대한 getter, setter 메소드와 순회 메소드
+    // 사칙연산 콜렉션
     public Queue<Double> getNumQ() {
         return numQ;
     }
@@ -32,6 +32,7 @@ public class Calculator {
             System.out.println(num);
         }
     }
+    // 원넓이 콜렉션
     public Queue<Double> getCircleQ() {
         return circleQ;
     }
--- src/CircleCalculator.java
+++ src/CircleCalculator.java
@@ -1,6 +1,3 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class CircleCalculator {
     // 원넓이
     static final double PI = 3.14159265358979323846;
