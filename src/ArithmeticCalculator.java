4f98ef18c92f53d3ccd789532d2b71be070457fc
2minus <blecker0610@gmail.com>
2024-04-30 22:10:19 +0900

    일단 정상 작동 ) Lv2.9 : 클래스 4가지 분기

--- src/ArithmeticCalculator.java
+++ src/ArithmeticCalculator.java
@@ -1,23 +1,26 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class ArithmeticCalculator {
+
+    private final AddOperator add = new AddOperator();
+    private final SubtractOperator sub = new SubtractOperator();
+    private final MultiplyOperator mul = new MultiplyOperator();
+    private final DivideOperator div = new DivideOperator();
+    private final ModOperator mod = new ModOperator();
+
     public double operate(double firstNumber, double secondNumber, char operator) throws Exception {
         // 연산자에 따른 연산방법
         switch (operator) {
             case '+':
-                return firstNumber + secondNumber;
+                return add.operate(firstNumber, secondNumber);
             case '-':
-                return firstNumber - secondNumber;
+                return sub.operate(firstNumber, secondNumber);
             case '*':
-                return firstNumber * secondNumber;
+                return mul.operate(firstNumber, secondNumber);
             case '/':
-                if (secondNumber != 0) {
-                    return firstNumber / secondNumber;
-                } else {
-                    throw new BadInputException("0이 아닌 정수");
-                }
-            default: throw new BadInputException("사칙연산자");
+                return div.operate(firstNumber, secondNumber);
+            case '%':
+                return mod.operate(firstNumber, secondNumber);
+            default:
+                throw new BadInputException("사칙연산자");
         }
 
         // 연산방법 제어 끝
---
+++ src/DivideOperator.java
@@ -0,0 +1,8 @@
+public class DivideOperator {
+    public double operate(double a, double b) throws Exception {
+        if (b == 0) {
+            throw new BadInputException("0이 아닌 정수");
+        }
+        return a / b;
+    }
+}
